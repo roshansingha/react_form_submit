5079e4de8f222842b0bc6cfb2d8aa19190d20475
roshansingha <roshansinghaagt@gmail.com>
2020-06-21 00:53:28 +0530

    form-submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      name:"",
+      password:"",
+    }
+  }
+
+  valid(item,type)
+  {
+    let itemValue= item.target.value;
+    switch(type)
+    {
+      case "name":{
+        if(itemValue.length<4)
+        {
+          item.target.style.color="red";
+          this.setState({name:itemValue})
+        }
+        else{
+          item.target.style.color="";
+          this.setState({name:itemValue})
+        }
+      }
+      case "password":{
+        if(itemValue.length<4)
+        {
+          item.target.style.color="red";
+          this.setState({name:itemValue})
+        }
+        else{
+          item.target.style.color="";
+          this.setState({name:itemValue})
+        }
+      }
+    }
+  }
+  submit()
+  {
+    let obj ={}
+    obj.name = this.state.name;
+    obj.password = this.state.password;
+    console.warn("submit data",obj)
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Reactjs Form Submission</h1>
+        <input type="text" placeholder="Enter Name" onChange={(item)=>this.valid(item,"name")}/>
+        <br/>
+        <input type="password" placeholder="Enter Password" onChange={(item)=> this.valid(item,"password")}/>
+        <br/>
+        <button onClick={()=>this.submit()}>Submit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
